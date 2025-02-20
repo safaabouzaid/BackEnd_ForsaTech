3730c0db0c5149ab1f35ce8214efdcf23c4733e4
Sedraalsabagh <sedraalsabbagh2021@gmail.com>
2025-02-20 16:02:09 +0300

    edit user model

--- devloper/serializer.py
+++ devloper/serializer.py
@@ -1,13 +1,12 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model  # استخدام النموذج المخصص
+from django.contrib.auth import get_user_model  
 
-User = get_user_model()  # اجلب نموذج المستخدم الحالي
-
+User = get_user_model()  
 class SingUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['email', 'username', 'password']
-        extra_kwargs = {'password': {'write_only': True}}  # لا تعرض كلمة المرور في الرد
+        extra_kwargs = {'password': {'write_only': True}} 
 
 
 class LoginSerializer(serializers.Serializer):
