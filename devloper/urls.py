00d68975001602babebcac217b695f29eac1bdc4
Sedraalsabagh <sedraalsabbagh2021@gmail.com>
2025-05-25 18:57:54 +0300

    fixBug

--- devloper/urls.py
+++ devloper/urls.py
@@ -2,14 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     # path('SignUp/',views.register,name='register'),
     path('LogIn/',views.login,name='register'),
     path('apply/<int:opportunity_id>/', views.apply_for_opportunity, name='apply-for-opportunity'),
-=======
-    path('SignUp/',views.register,name='register'),
-    path('LogIn/',views.login,name='login'),
-    path('complaints/', views.add_complaint, name='add_complaint'),
->>>>>>> 791cbf3cd573cd588591bcc0c1dbb4b8748675b9
+
+
     
 ]
