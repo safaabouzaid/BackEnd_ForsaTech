9577e49ff24f44b6d1d719495b451f68851cef52
safaabouzaid <abouzaidsafa@gmail.com>
2025-06-27 21:57:12 +0300

    .

--- Recommendation/apps.py
+++ Recommendation/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class RecommendationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Recommendation'
+
+    def ready(self):
+        from .utils import get_sbert_model
+        get_sbert_model()
