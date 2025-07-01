681c9784eafcb3deff7f8d1c7392917f0d3fb7e8
safaabouzaid <abouzaidsafa@gmail.com>
2025-07-01 14:08:57 +0300

    fix/opp. embedding

--- Recommendation/tasks.py
+++ Recommendation/tasks.py
@@ -1,12 +1,22 @@
-from celery import shared_task
+
 from .utils import get_sbert_model, get_opportunity_vector
 from human_resources.models import Opportunity
 import numpy as np
 
-@shared_task
+# @shared_task
 def generate_opportunity_embedding(opp_id):
     opp = Opportunity.objects.get(pk=opp_id)
     vec = get_opportunity_vector(opp, get_sbert_model())
     if np.linalg.norm(vec) > 0:
         opp.embedding = vec.tolist()
         opp.save()
+
+
+def generate_all_opportunity_embeddings():
+    model = get_sbert_model()
+    opportunities = Opportunity.objects.all()
+    for opp in opportunities:
+        vec = get_opportunity_vector(opp, model)
+        if np.linalg.norm(vec) > 0:
+            opp.embedding = vec.tolist()
+            opp.save()
