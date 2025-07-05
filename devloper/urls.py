7f0957f0fe9224124dd11e3bb4c5dff1cc257b67
safaabouzaid <abouzaidsafa@gmail.com>
2025-07-05 17:26:20 +0300

    fix/sign up

--- devloper/urls.py
+++ devloper/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('SignUp/',views.register,name='register'),
+    path('SignUp/',views.register,name='register'),
     path('LogIn/',views.login,name='register'),
     path('languages/save/', views.save_languages, name='save_languages'),
     path('developer/add/', views.add_developer, name='add-developer'),
