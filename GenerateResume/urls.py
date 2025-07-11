326136b1f444d648fbc0e32609238a546d4afbec
Sedraalsabagh <sedraalsabbagh2021@gmail.com>
2025-07-11 13:18:49 +0300

    fixBug

--- GenerateResume/urls.py
+++ GenerateResume/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ResumeAPIView
+from .views import ATSResumeConverterAPIView, ResumeAPIView
 
 urlpatterns = [
     path('generate-resume/', ResumeAPIView.as_view(), name='generate_resume'),
+    path('convert-ats-resume/', ATSResumeConverterAPIView.as_view(), name='convert-ats-resume'),
 
 
 ]
